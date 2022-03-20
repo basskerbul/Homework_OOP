00d3ab08808e1ca73488199dd42b3904e609a4b2
NaGiBaToR9000 <wpusti@yandex.ru>
2022-03-20 11:50:51 +0300

    lesson3.2

--- Homework_OOP/Program.cs
+++ Homework_OOP/Program.cs
@@ -8,8 +8,25 @@ namespace Homework_OOP
 {
     internal class Program
     {
+        static string FlipLine(string str)
+        {
+            string NewStr = "";
+            for(int i = str.Length - 1; i >= 0; i--)
+            {
+                NewStr += str[i];
+            }
+            return NewStr;
+        }
+
         static void Main(string[] args)
         {
+            //Реализовать метод, который в качестве входного параметра принимает строку string, 
+            //возвращает строку типа string, буквы в которой идут в обратном порядке. Протестировать метод.
+
+            string mystr = "Hello, World!";
+            Console.WriteLine(mystr);
+            Console.WriteLine(FlipLine(mystr));
+            Console.ReadKey();
         }
     }
 }
